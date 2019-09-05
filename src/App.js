3a75b0744ed28c1a81ced4417a862eee38556472
Alaeddine Messadi <alaeddine.messadi@smava.de>
2019-09-05 10:57:43 +0200

    INIT

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from 'material-kit-react/src/components/CustomButtons';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="button" color="warning">Warning</Button>
+    </div>
+  );
+}
+
+export default App;
